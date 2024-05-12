3c5b977bbe0b6b0deddba37b36aa01ae177ca026
TienHien4 <kurumilop8@gmail.com>
2024-05-12 08:16:36 +0700

    NguyenTienHien

---
+++ src/Test/Main.java
@@ -0,0 +1,29 @@
+
+package Test;
+
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+import view.FrameDangNhap;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrameDangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrameDangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrameDangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrameDangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        FrameDangNhap dn=new FrameDangNhap();
+        dn.setVisible(true);
+    }
+}
---
+++ src/controller/QuanLyDangNhap.java
@@ -0,0 +1,53 @@
+
+package controller;
+
+import java.util.ArrayList;
+import model.NhanVien;
+import view.FrameDangNhap;
+import view.FrameNhanVien;
+import view.FrameQuanLy;
+
+
+public class QuanLyDangNhap {
+    
+    QuanLyNhanVien qlnv;
+    ArrayList<NhanVien> ds_NhanVien;
+    public static boolean isQuanLi = false;
+    public static boolean isNhanVien = false;
+    
+    public QuanLyDangNhap(){
+        qlnv = new QuanLyNhanVien();
+        ds_NhanVien =qlnv.getDS_NhanVien();
+    }
+    
+    public int Login(String username, String password) {
+    	String ma = "1";
+        for (int i = 0; i < ds_NhanVien.size(); i++) {
+            if (username.equals(ds_NhanVien.get(i).getTenDangNhap()) && password.equals((ds_NhanVien.get(i).getMatKhau()))) {
+                if (ds_NhanVien.get(i).getMaNhanVien().compareTo(ma)==0){
+                    isQuanLi = true; isNhanVien = false;
+                    return 0;
+                } else {
+                    isQuanLi = false; isNhanVien = true;
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    
+    public void login(int type,String user){
+        if (type == 1){
+            FrameNhanVien fr = new FrameNhanVien(user);
+            fr.setVisible(true);
+        } if(type == 0) {
+            FrameQuanLy fr = new FrameQuanLy();
+            fr.setVisible(true);
+        }
+    }
+    
+    public void logout(){
+        FrameDangNhap fr = new FrameDangNhap();
+        fr.setVisible(true);
+    }
+}
